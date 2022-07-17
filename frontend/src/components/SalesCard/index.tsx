a0a6c9f720881e28a96e53ebb58e5d328deb397a
onvito <lvictor462@gmail.com>
2022-07-16 23:04:53 -0300

    alterando title projeto

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -52,7 +52,7 @@ function SalesCard() {
               <tr>
                 <td className="show992">#341</td>
                 <td className="show576">08/07/2022</td>
-                <td>Anakin</td>
+                <td>Celin</td>
                 <td className="show992">15</td>
                 <td className="show992">11</td>
                 <td>R$ 55300.00</td>
@@ -65,7 +65,7 @@ function SalesCard() {
               <tr>
                 <td className="show992">#341</td>
                 <td className="show576">08/07/2022</td>
-                <td>Anakin</td>
+                <td>Celin</td>
                 <td className="show992">15</td>
                 <td className="show992">11</td>
                 <td>R$ 55300.00</td>
@@ -78,7 +78,7 @@ function SalesCard() {
               <tr>
                 <td className="show992">#341</td>
                 <td className="show576">08/07/2022</td>
-                <td>Anakin</td>
+                <td>Celin</td>
                 <td className="show992">15</td>
                 <td className="show992">11</td>
                 <td>R$ 55300.00</td>
